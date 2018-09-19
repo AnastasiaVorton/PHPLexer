14b59fb4352bd1144b861c226a55552b6e87314b
Bulat Khabirov <khabiroff_b@me.com>
2018-09-19 04:11:41 +0300

    Added more interfaces and documentation

--- src/main/java/core/LexicalAnalyzer.java
+++ src/main/java/core/LexicalAnalyzer.java
@@ -1,5 +1,9 @@
 package core;
 
-public interface LexicalAnalyzer {
-    Token getNextToken();
+/**
+ * An interface describing a generic lexical analyzer. It parses the source code into a sequence of tokens.
+ * @param <T> Actual type of TokenInstance to use.
+ */
+public interface LexicalAnalyzer<T extends TokenInstance> {
+    T getNextToken();
 }
---
+++ src/main/java/phplexer/PHPLexicalAnalyzer.java
@@ -0,0 +1,13 @@
+package phplexer;
+
+import core.LexicalAnalyzer;
+
+/**
+ * Lexical analyzer for PHP language.
+ */
+public class PHPLexicalAnalyzer implements LexicalAnalyzer<PHPTokenInstance> {
+    @Override
+    public PHPTokenInstance getNextToken() {
+        return null; // TODO: add proper implementation
+    }
+}
