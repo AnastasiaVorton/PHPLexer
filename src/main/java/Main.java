6599a23822fa0540e13197cecc7d19640d96ab88
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-09-20 16:42:38 +0300

    Trying implementing lexer, regexp fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import phplexer.Lexer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+//        System.out.println("hello");
+        Lexer lexer = new Lexer("in.txt");
+        System.out.println(lexer.input);
+        while (!lexer.isExausthed()) {
+            System.out.println("lexema: " + lexer.currentLexema() + ", token: " + lexer.currentToken());
+            lexer.moveAhead();
+        }
 
+        if (lexer.errorMessage() != null && !lexer.errorMessage().isEmpty()) {
+            System.out.println(lexer.errorMessage());
+        } else {
+            System.out.println("No errors found");
+        }
     }
 }
--- src/main/java/phplexer/PHPToken.java
+++ src/main/java/phplexer/PHPToken.java
@@ -6,16 +6,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum PHPToken implements Token {
-    // TODO ALWAYS ESCAPE (\\) BEFORE +, *, |, (, ), {, } ETC
+    // TODO ALWAYS ESCAPE (\\) BEFORE +, *, |, (, ), {, }, [, ] ETC
     T_ABSTRACT("abstract"), // class abstraction (abstract)
     T_AND_EQUAL("&="),  // bitwise and assignment operator (&=)
     T_ARRAY("array\\("), // array declaration ( array() )
     T_ARRAY_CAST("(array)"), // casting to array ( (array) )
     T_AS("as"), // foreach operator (as)
-    T_BAD_CHARACTER(""), // anything below ASCII 32 except \t (0x09), \n (0x0a) and \r (0x0d) TODO regexp (this is how Pattern defines all ASII "\p{ASCII}	All ASCII:[\x00-\x7F])"
+//    T_BAD_CHARACTER(""), // anything below ASCII 32 except \t (0x09), \n (0x0a) and \r (0x0d) TODO regexp (this is how Pattern defines all ASII "\p{ASCII}	All ASCII:[\x00-\x7F])"
     T_BOOLEAN_AND("&&"), // logical and operator (&&)
     T_BOOLEAN_OR("\\|\\|"), // logical or operator (||)
-    T_BOOL_CAST("[\\(bool\\)|\\(boolean\\)]"), // casting to boolean ( (bool) or (boolean) )
+    T_BOOL_CAST("\\(bool\\)|\\(boolean\\)"), // casting to boolean ( (bool) or (boolean) )
     T_BREAK("break"), // break operator (break)
     T_CALLABLE("callable"), // callable keyword (callable)
     T_CASE("case"), // switch-case operator (case)
@@ -23,28 +23,28 @@ public enum PHPToken implements Token {
     T_CLASS("class"), // class declaration keyword (class)
     T_CLASS_C("__CLASS__"), // class magic constant (__CLASS__)
     T_CLONE("clone"), // object cloning operator (clone)
-    T_CLOSE_TAG(""), // escaping from HTML tag (?> or %>)
-    T_COALESCE("??"), // null Coalescing operator (??)
-    T_COMMENT("[//|#|/\\*[smt]\\*/]"), // comments (// or # and /**/) TODO in smt
-    T_CONCAT_EQUAL(".="), // concatenation assignment operator (.=)
+    T_CLOSE_TAG("\\?\\>|%\\>"), // escaping from HTML tag (?> or %>)
+    T_COALESCE("\\?\\?"), // null Coalescing operator (??)
+    T_COMMENT("//|#|/\\*[smt]\\*/"), // comments (// or # and /**/) TODO in smt
+    T_CONCAT_EQUAL("\\.="), // concatenation assignment operator (.=)
     T_CONST("const"), // class constant (const)
-    T_CONSTANT_ENCAPSED_STRING(""), // TODO meaning? just a string?
+//    T_CONSTANT_ENCAPSED_STRING(""), // TODO meaning? just a string?
     T_CONTINUE("continue"), // continue keyword (continue)
-    T_CURLY_OPEN("\\{$"), // passing complex variables as inputs ({$)
+    T_CURLY_OPEN("\\{\\$"), // passing complex variables as inputs ({$)
     T_DEC("--"), // decrementing operator (--)
     T_DECLARE("declare"), // declare keyword (declare)
     T_DEFAULT("default"), // default keyword for switch-case (default)
     T_DIR("__DIR__"), // directory magic constant (__DIR__)
     T_DIV_EQUAL("/="), // division assignment  operator (/=)
     T_DNUMBER("(\\d+)\\.\\d+"), // double numbers (0.21 etc)
-    T_DOC_COMMENT("[/\\*\\*\\p{Alnum}*\\*/]"), // docstyle comment (/** */) TODO check
+    T_DOC_COMMENT("[/\\*\\*\\w+\\*\\*/]"), // docstyle comment (/** */) TODO check
     T_DO("do"), // do keyword for while statement (do)
-    T_DOLLAR_OPEN_CURLY_BRACES("$\\{"), //passing complex variables as inputs (${)
+    T_DOLLAR_OPEN_CURLY_BRACES("\\$\\{"), //passing complex variables as inputs (${)
     T_DOUBLE_ARROW("=>"), // array key => value assignment (=>)
-    T_DOUBLE_CAST("[\\(real\\)|\\(double\\)|\\(float\\)]"), // casting to double ( (real), (double) or (float) )
+    T_DOUBLE_CAST("\\(real\\)|\\(double\\)|\\(float\\)"), // casting to double ( (real), (double) or (float) )
     T_DOUBLE_COLON("::"), // (::) TODO exaplain where used
     T_ECHO("echo"), // output strings (echo)
-    T_ELLIPSIS("..."), // a variable number of arguments in a function (...)
+    T_ELLIPSIS("\\.\\.\\."), // a variable number of arguments in a function (...)
     T_ELSE("else"), // else keyword (else)
     T_ELSEIF("elseif"), // elseif keyword (elseif)
     T_EMPTY("empty"), // determine whether a variable is empty (empty)
@@ -55,16 +55,16 @@ public enum PHPToken implements Token {
     T_ENDIF("endif"), // endif keyword (endif)
     T_ENDSWITCH("endswitch"), // endswitch keyword (endswitch)
     T_ENDWHILE("endwhile"), // endwhile keyword (endwhile)
-    T_END_HEREDOC(""), // a way to delimit strings () TODO how it's represented?
+//    T_END_HEREDOC(""), // a way to delimit strings () TODO how it's represented?
     T_EVAL("eval\\(\\)"), // evaluate a string as PHP code (eval()) TODO whats in brackets?
-    T_EXIT("[exit|die]"), // output a message and terminate the current script (exit or die)
+    T_EXIT("exit|die"), // output a message and terminate the current script (exit or die)
     T_EXTENDS("extends"), // inheritance keyword (extends)
     T_FILE("__FILE__"), // file magic constant (__FILE__)
     T_FINAL("final"), // final keyword (final)
     T_FINALLY("finally"), // an exception block specified after or instead of catch blocks (finally)
     T_FOR("for"), // for keyword (for)
     T_FOREACH("foreach"), // foreach keyword (foreach)
-    T_FUNCTION("[function|cfunction]"), // function declaration keyword (function or cfunction)
+    T_FUNCTION("function|cfunction"), // function declaration keyword (function or cfunction)
     T_FUNC_C("__FUNCTION__"), // function magic constant (__FUNCTION__)
     T_GLOBAL("global"), // variable scope (global)
     T_GOTO("goto"), // goto keyword (goto)
@@ -74,16 +74,16 @@ public enum PHPToken implements Token {
     T_INC("\\+\\+"), // incrementing operator (++)
     T_INCLUDE("include\\("), // include keyword (include()) TODO whats in brackets?
     T_INCLUDE_ONCE("include_once\\("), // includes and evaluates the file during the execution of the script (include_once()) TODO whats in brackets?
-    T_INLINE_HTML("[<\\w+/>]"), // text outside PHP (HTML text) TODO
+//    T_INLINE_HTML("[<\\w+/>]"), // text outside PHP (HTML text) TODO
     T_INSTANCEOF("instanceof"), // instance of a class (instanceof)
     T_INSTEADOF("insteadof"), // trait (code reuse) conflict resolution keyword (insteadof)
-    T_INT_CAST("[(int)|(integer)]"), // casting to integer ( (int) or (integer) )
+    T_INT_CAST("(int)|(integer)"), // casting to integer ( (int) or (integer) )
     T_INTERFACE("interface"), // interface keyword (interface)
     T_ISSET("isset\\("), // determine if a variable is set and is not NULL (isset()) TODO
     T_IS_EQUAL("=="), // comparison operator (==)
     T_IS_GREATER_OR_EQUAL(">="), // comparison operator (>=)
     T_IS_IDENTICAL("==="), // comparison operator (===)
-    T_IS_NOT_EQUAL("[!=|<>]"), // comparison operator (!= or <>)
+    T_IS_NOT_EQUAL("!=|\\<\\>"), // comparison operator (!= or <>)
     T_IS_NOT_IDENTICAL("!=="), // comparison operator (!==)
     T_IS_SMALLER_OR_EQUAL("<="), // comparison operator (<=)
     T_SPACESHIP("<=>"), // comparison operator (<=>)
@@ -95,17 +95,17 @@ public enum PHPToken implements Token {
     T_LOGICAL_XOR("xor"), // logical xor operator (xor)
     T_METHOD_C("__METHOD__"), // magic constant for method (__METHOD__)
     T_MINUS_EQUAL("-="), // minus equal operator (-=)
-    T_MOD_EQUAL("%="), // mod equal operator ($=)
+    T_MOD_EQUAL("%="), // mod equal operator (%=)
     T_MUL_EQUAL("\\*="), // multiply equal operator (*=)
     T_NAMESPACE("namespace"), // namespace keyword ()
     T_NS_C("__NAMESPACE__"), // magic constant for namespace
-    T_NS_SEPARATOR("\\"), // namespace separator TODO should be "\"
+//    T_NS_SEPARATOR("[\]"), // namespace separator TODO should be "\"
     T_NEW("new"), // new keyword
-    T_NUM_STRING("$\\w+[\\d+]"), // numeric array index inside string
+    T_NUM_STRING("\\$\\w+[\\d+]"), // numeric array index inside string
     T_OBJECT_CAST("\\(object\\)"), // casting to object
     T_OBJECT_OPERATOR("->"), // object operator
-    T_OPEN_TAG("[<?php|<?|<%]"), // escaping from HTML
-    T_OPEN_TAG_WITH_ECHO("[<?=|<%=]"), // escaping from HTML
+    T_OPEN_TAG("\\<\\?php|<\\?|\\<%"), // escaping from HTML
+    T_OPEN_TAG_WITH_ECHO("[<\\?=|<%=]"), // escaping from HTML
     T_OR_EQUAL("\\|="), // bitwise or assignment operator
     T_PLUS_EQUAL("\\+="), // plus assignment operator
     T_POW("\\*\\*"), // power arithmetic operator
@@ -125,7 +125,7 @@ public enum PHPToken implements Token {
     T_STATIC("static"), // scope keyword
     T_STRING("\\w+"), // identifiers, e.g. keywords like parent and self, function names, class names and more are matched. See also T_CONSTANT_ENCAPSED_STRING. TODO nee one of those
     T_STRING_CAST("\\(string\\)"), // casting to string
-    T_STRING_VARNAME("$\\{\\w+"), // including vars, arr elements etc as a string
+    T_STRING_VARNAME("\\$\\{\\w+"), // including vars, arr elements etc as a string
     T_SWITCH("switch"), // condition keyword
     T_THROW("throw"), // exception keyword
     T_TRAIT("trait"), // trait keyword
@@ -135,7 +135,7 @@ public enum PHPToken implements Token {
     T_UNSET_CAST("\\(unset\\)"), // casting to unset
     T_USE("use"), // use keyword
     T_VAR("var"), // var keyword
-    T_VARIABLE("$\\w+"), // variable declaration
+    T_VARIABLE("\\$\\w+"), // variable declaration
     T_WHILE("while"), // while keyword
     T_WHITESPACE("[\t|\r|\n]"), // whitespaces TODO
     T_XOR_EQUAL("^="), // xor assignment
